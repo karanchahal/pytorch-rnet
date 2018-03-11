15d7301ed0b25e83c0afd4e36d1cc32ac476e9c9
karanchahal <karanbleep@gmail.com>
2018-03-11 18:50:32 +0530

    Updated gitignore

---
+++ preprocess.py
@@ -0,0 +1,59 @@
+import json
+from word_model import Vocab ,WordModel
+import pickle
+import coloredlogs, logging
+from encoder import Encoder
+from torch.autograd import Variable
+
+# Create a logger object.
+logger = logging.getLogger(__name__)
+
+coloredlogs.install(level='DEBUG')
+coloredlogs.install(fmt='%(asctime)s,%(msecs)03d %(levelname)s %(message)s')
+
+dataset = json.load(open('data/dev-v1.1.json'))
+
+word_model = WordModel()
+logger.warning('Loading Vocab ...')
+word_model.store_into_vocab(dataset)
+word_model.load_vocab()
+vocab_size = word_model.vocab.length()
+encoder = Encoder(vocab_size=vocab_size)
+
+def train_model(context,question,answer):
+    context,question,answer = Variable(context),Variable(question),Variable(answer)
+    context = context.unsqueeze(0)
+    question = question.unsqueeze(0)
+    answer = answer.unsqueeze(0)
+    encoder.initHidden(question)
+
+    s,e = encoder(context,question)
+    
+    print(s)
+    print(e)
+
+for input in dataset['data']:
+    for paragraph in input['paragraphs']:
+        context = paragraph['context']
+        context_ids = word_model.parse(context)
+
+        questions_answers = paragraph['qas']
+        for qa in questions_answers:
+            print(qa['question'])
+            question = qa['question']
+            question_ids = word_model.parse(question,qa=True)
+
+            answers = qa['answers']
+            for ans in answers:
+                ans_start = ans['answer_start']
+                ans_text = ans['text']
+                ans_end = ans_start + len(ans_text)
+                # print(context[ans_start:ans_end])
+                ans_ids = word_model.parse(ans_text,qa=True)
+            
+                train_model(context_ids,question_ids,ans_ids)
+
+                break
+            break
+        break
+    break
